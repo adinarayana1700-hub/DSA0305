e7ce81c1d11282ab19c34f79c08829adc4b0eca8
adinarayana1700-hub <adinarayana1700@gmail.com>
2026-09-01 08:39:52 +0530

    Create exp 19.py

---
+++ LAB/exp 19.py
@@ -0,0 +1,11 @@
+from nltk.wsd import lesk
+from nltk.tokenize import word_tokenize
+sentence = input("Enter sentence: ")
+word = input("Enter ambiguous word: ")
+tokens = word_tokenize(sentence)
+sense = lesk(tokens, word)
+if sense:
+    print("Selected Sense:", sense.name())
+    print("Definition:", sense.definition())
+else:
+    print("Sense not found")
